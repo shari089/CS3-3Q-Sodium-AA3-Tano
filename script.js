1764a6401d622576aa7157944224d3de0332c672
shari089 <sybtano22@src.pshs.edu.ph>
2025-03-10 10:41:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ function verifyMessage() {
   let message = prompt("Enter a message");
   let print = document.getElementById('result');
 
-  if(message.indexOf("aI") || message.indexOf("Ai")) {
+  if(message.indexOf("aI") !== -1 || message.indexOf("Ai")) !== -1 {
       print.textContent = " is tampered with";
   } 
-  else if(message.indexOf("fR") || message.indexOf("Fr")) {
+  else if(message.indexOf("fR") !== -1 || message.indexOf("Fr")) !== -1 {
       print.textContent = " is tampered with";
   } 
-  else if(message.indexOf("AI")) {
+  else if(message.indexOf("AI") !== -1 ) {
       print.textContent = " is legitimate";
   } 
-  else if(message.indexOf("FR")) {
+  else if(message.indexOf("FR") !== -1 ) {
       print.textContent = " is legitimate";
   } 
-  else if(message.indexOf("FR") && message.indexOf("AI")) {
+  else if(message.indexOf("FR") !== -1 && message.indexOf("AI") !== -1 ) {
       print.textContent = " is legitimate";
   } 
   else {
