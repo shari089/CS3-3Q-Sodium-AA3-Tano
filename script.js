ea1b171d4c0d546b5cb28012282558a76c4c4fe5
shari089 <sybtano22@src.pshs.edu.ph>
2025-03-10 10:07:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function verifyMessage() {
+  let message = prompt("Enter a message");
+  let print = document.getElementById('result');
 
+  if(message.indexOf("FR") && message.indexOf("AI")) {
+      print.textContent = " is legitimate";
+  } else if(message.indexOf("FR")) {
+      print.textContent = " is legitimate";
+  } else if(message.indexOf("AI")) {
+      print.textContent = " is legitimate";
+  } else if(message.indexOf("aI") || message.indexOf("Ai")) {
+      print.textContent = " is tampered with";
+  } else if(message.indexOf("fR") || message.indexOf("Fr")) {
+      print.textContent = " is tampered with";
+  } else {
+      print.textContent = " is not yet encoded";
+  }
+
+  document.getElementById('result').innerHTML = "The message" + " " + message + " " + print.textContent;
+}
